ea71843982a50a58d4d04f620543901fe351e010
K3cs1 <kecskemetip@gmail.com>
2026-01-16 07:17:17 +0100

    add not null

--- backend/src/main/java/org/k3cs1/subtitletranslatorapp/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/k3cs1/subtitletranslatorapp/exception/GlobalExceptionHandler.java
@@ -2,16 +2,18 @@ package org.k3cs1.subtitletranslatorapp.exception;
 
 import org.k3cs1.subtitletranslatorapp.api.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public static ResponseEntity<ApiResponse<?>> errorResponseEntity(String message, HttpStatus status) {
+    public static ResponseEntity<ApiResponse<?>> errorResponseEntity(String message, @NonNull HttpStatusCode status) {
         ApiResponse<?> response = ApiResponse.error(message);
-        return new ResponseEntity<>(response, status);
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
