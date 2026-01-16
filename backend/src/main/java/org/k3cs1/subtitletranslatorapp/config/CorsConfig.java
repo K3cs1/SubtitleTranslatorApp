2d541f0537def2656c1e9efdfbdc16e3f2efd354
Pal Kecskemeti <kecskemetip@gmail.com>
2026-01-16 14:52:45 +0100

    update cors config

--- backend/src/main/java/org/k3cs1/subtitletranslatorapp/config/CorsConfig.java
+++ backend/src/main/java/org/k3cs1/subtitletranslatorapp/config/CorsConfig.java
@@ -1,6 +1,7 @@
 package org.k3cs1.subtitletranslatorapp.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,9 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://d1yzzvrnimz8tw.cloudfront.net",
+                        "https://dy455l8ky4gpi.cloudfront.net"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
